0f582a1fd81b00008b3a37645057ed66eb123c99
Churikov-SV <Churikov.SV@outlook.com>
2022-01-22 22:57:44 +0300

    TASK_01

--- internetshop/views.py
+++ internetshop/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-
 def products(request):
     return render(request, 'products.html')
-
 
 def contact(request):
     return render(request, 'contact.html')
