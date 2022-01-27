59926cd9ff3df1d8017a3a1ab55d6e9525fe1510
Churikov-SV <Churikov.SV@outlook.com>
2022-01-27 18:25:39 +0300

    TASK_03

--- internetshop/views.py
+++ internetshop/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
+from datetime import timedelta
+from django.utils import timezone
 
 # Create your views here.
 
+MENU_LINKS = [
+    {'url': 'main', 'name': 'домой'},
+    {'url': 'products', 'name': 'продукты'},
+    {'url': 'contact', 'name': 'контакты'},
+]
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context = {
+        'variable': 'hello text',
+        'datetime': timezone.now() + timedelta(days=1),
+        'title': 'Главная',
+        'menu_links': MENU_LINKS
+    })
 
 def products(request):
-    return render(request, 'products.html')
+    return render(request, 'products.html', context = {
+        'title': 'Продукты',
+        'menu_links': MENU_LINKS
+    })
 
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact.html', context = {
+        'title': 'Продукты',
+        'menu_links': MENU_LINKS
+    })
